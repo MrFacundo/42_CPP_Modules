d8a03dfb96bf0c46de55e40d17ce44a8e6ec0665
MrFacundo <ftroitero@gmail.com>
2024-05-08 23:10:06 +0100

    cpp8 ex00 header

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -3,66 +3,61 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: facu <facu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: ftroiter <ftroiter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 19:42:18 by facu              #+#    #+#             */
-/*   Updated: 2024/05/07 19:54:03 by facu             ###   ########.fr       */
+/*   Updated: 2024/05/08 23:08:46 by ftroiter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BITCOINEXCHANGE_HPP
-# define BITCOINEXCHANGE_HPP
+#include "BitcoinExchange.hpp"
 
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <ctime>
-#include <map>
+// CONSTRUCTORS AND DESTRUCTORS
+BitcoinExchange::BitcoinExchange(void)
+{
+}
 
-class BitcoinExchange {
-	public:
-		// CONSTRUCTORS AND DESTRUCTORS
-		BitcoinExchange(void);
-		BitcoinExchange(BitcoinExchange const &src);
-		~BitcoinExchange(void);
-		// OPERATORS
-		BitcoinExchange& operator=(BitcoinExchange const &rhs);
-		// MEMBER FUNCTIONS
-		void loadDB(std::string const &filename);
-		void loadInput(std::string const &filename);
-		void printExchange(void);
+BitcoinExchange::BitcoinExchange(BitcoinExchange const &src)
+{
+	*this = src;
+}
 
-		class DBParsingException : public std::exception {
-			public:
-				virtual const char* what(void) const throw()
-				{
-					return ("Error: DBParsingException");
-				}
-		};
-		class BadInputException : public std::exception {
-			public:
-				virtual const char* what(void) const throw()
-				{
-					return ("Error: BadInputException");
-				}
-		};
-		class BadValueException : public std::exception {
-			public:
-				virtual const char* what(void) const throw()
-				{
-					return ("Error: BadValueException");
-				}
-		};
-		class BadDateException : public std::exception {
-			public:
-				virtual const char* what(void) const throw()
-				{
-					return ("Error: BadDateException");
-				}
-		};
-	private:
-		
-};
+BitcoinExchange::~BitcoinExchange(void)
+{
+}
 
-#endif
+// OPERATORS
+
+BitcoinExchange &BitcoinExchange::operator=(BitcoinExchange const &rhs)
+{
+	if (this != &rhs)
+	{
+		this->values = rhs.getValues();
+		this->transactions = rhs.getTransactions();
+		this->prices = rhs.getPrices();
+	}
+	return *this;
+}
+
+// MEMBER FUNCTIONS
+
+void BitcoinExchange::loadPrices(std::string const &filename)
+{
+	std::ifstream file(filename);
+	std::string line;
+	std::string date;
+	float value;
+	std::vector<Price> prices;
+
+	if (!file.is_open())
+		throw PricesParsingException("Could not open file");
+
+	while (std::getline(file, line))
+	{
+		std::istringstream iss(line);
+		if (!(iss >> date >> value))
+			throw PricesParsingException("Could not parse line");
+		prices.push_back({date, value});
+	}
+	this->prices = prices;
+}
---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ftroiter <ftroiter@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 22:15:11 by ftroiter          #+#    #+#             */
+/*   Updated: 2024/05/08 23:08:48 by ftroiter         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <iterator>
+
+struct Transaction
+{
+	std::string date;
+	float value;
+};
+
+struct Price
+{
+	std::string date;
+	float value;
+};
+
+class BitcoinExchange
+{
+public:
+	// CONSTRUCTORS AND DESTRUCTORS
+	BitcoinExchange(void);
+	BitcoinExchange(BitcoinExchange const &src);
+	~BitcoinExchange(void);
+	
+	// OPERATORS
+	BitcoinExchange &operator=(BitcoinExchange const &rhs);
+
+	// MEMBER FUNCTIONS
+	void loadPrices(std::string const &filename);
+	void loadTransactions(std::string const &filename);
+
+	// GETTERS
+	std::unordered_map<std::string, float> getValues() const;
+	std::vector<Transaction> getTransactions() const;
+	std::vector<Price> getPrices() const;
+	
+	// EXCEPTIONS
+	class PricesParsingException : public std::exception
+	{
+	public:
+		PricesParsingException(const std::string &message) : message("PricesParsingException: " + message) {}
+		virtual const char *what() const throw()
+		{
+			return message.c_str();
+		}
+
+	private:
+		std::string message;
+	};
+	class TransactionsParsingException : public std::exception
+	{
+	public:
+		TransactionsParsingException(const std::string &message) : message("TransactionsParsingException: " + message) {}
+		virtual const char *what() const throw()
+		{
+			return message.c_str();
+		}
+
+	private:
+		std::string message;
+	};
+	class BadInputException : public std::exception
+	{
+	public:
+		BadInputException(const std::string &message) : message("BadInputException: " + message) {}
+		virtual const char *what() const throw()
+		{
+			return message.c_str();
+		}
+
+	private:
+		std::string message;
+	};
+	class BadDateException : public std::exception
+	{
+	public:
+		BadDateException(const std::string &message) : message("BadDateException: " + message) {}
+		virtual const char *what() const throw()
+		{
+			return message.c_str();
+		}
+
+	private:
+		std::string message;
+	};
+
+private:
+	std::unordered_map<std::string, float> values;
+	std::vector<Transaction> transactions;
+	std::vector<Price> prices;
+};
+
+#endif
